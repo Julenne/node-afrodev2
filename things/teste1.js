05421c1ea9ea052f11ab29972ef7cfe679dcf69d
Arlene Pelenda <arlenej011@gmail.com>
2021-04-06 20:04:45 -0300

    desafio1

---
+++ things/teste1.js
@@ -0,0 +1,39 @@
+(async function(){
+	const listaCompras = { 
+		batata: 13, 
+		sabao: 3,
+		abobrinha: 5, 
+		toalha: 1,
+		cenoura: 8, 
+		balas: 10,
+		xuxu: 0 
+	} 
+
+console.log('------------> Desafio 1 <---------------')
+
+const sleep = ms => {
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const pegarQtdItem = async (item) => { 
+	await sleep(5000);
+	return listaCompras[item] 
+} 
+
+const looping = async _ => {
+	try {
+		console.log('Começou')
+		for (const key of Object.keys(listaCompras)) {
+			const qtdItem = await pegarQtdItem(key)
+			console.log(key, qtdItem);
+		}
+		console.log('Terminou')
+	} catch( error) {
+		console.log()
+	}
+}
+
+looping()
+})()
+//crie uma função q receba o item, utilize a função sleep para esperar 5 seg e retorne a qntd do item 
+//2 completar a função looping para printar na tela conforme a seguir
