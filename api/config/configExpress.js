0eee6825995d65316720638f27f3f1af9d714ed6
Arlene Pelenda <arlenej011@gmail.com>
2021-04-09 22:07:26 -0300

    aula

--- api/config/configExpress.js
+++ api/config/configExpress.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = require('../routes/agendamentos');
-
+const FormatoInvalido = require('../errors/FormatoInvalido')
+const FormatosValidos = require('../Serializar').FormatosValidos;
 module.exports = () => {
   const app = express();
+
+  app.use((req, resp, next) => {
+    let FormatoSolicitado = req.header('Accept')
+    if(FormatoSolicitado === '*/*'){
+      formatoSolicitado = 'application/json'
+    }
+    if(FormatosValidos.indexOf(formatoSolicitado) === -1){ //-1 = não está
+      resp.status(406);
+      return resp.send();
+    }
+
+    resp.setHeader('Content-Type', formatoSolicitado);
+    next();
+  });
 
   app.use(express.json())
   app.use('/api', router) // descrição da rota
--- api/routes/agendamentos/index.js
+++ api/routes/agendamentos/index.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
 const TabelaAgendamento = require('../../agendamentos/TabelaAgendamento')
 const Agendamento = require('../../agendamentos/Agendamento')
+const SerializadorAgendamento = require('../../Serializar').SerializarAgendamento;
 
 router.get('/agendamentos', async (req, resp) => {
-    const results = await TabelaAgendamento.listar()
-    resp.send(JSON.stringify(results)) 
+    try{
+        const results = await TabelaAgendamento.listar()
+        const serializador = new SerializadorAgendamento(
+            resp.getHeader('Content-Type'),
+            ['nome_servico','status']
+        );
+        agendamentos = serializador.transformar(results)
+        resp.status(200).send(agendamentos);
+    } catch(error) {
+        resp.send(error)
+    }
+     
 });
 
 router.post('/agendamentos', async (req, resp) => {
